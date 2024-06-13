df7ac03daed3948207d1bf950c49b07ae89497e0
Zxc-Ibrokhim <161280335+Zxc-Ibrokhim@users.noreply.github.com>
2024-06-13 12:58:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+// let userName  = +prompt("1) imtihon \n 2) coin \n 3) park \n 4) bowling ")
+// switch (userName){
+//     case 1:
+//         console.log("Imtihondan otingiz");
+//         break;
+//     case 2:
+//         console.log("Coin muborak");
+//         break;
+//     case 3:
+//         console.log("park muborak");
+//         break;
+//         case 4:
+//             console.log("MEGA PLANET");
+//             break;
+//     default:
+//         console.log("notogri raqam");
+// }
+
+
+// let userName = +prompt("Введите число ")
+// let newText = document.createElement("p")
+
+// switch(userName){
+//     case 1:
+//         newText.textContent = "Monday";
+//         document.body.append(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+//     case 2:
+//         newText.textContent = "Tuesday";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+//     case 3:
+//         newText.textContent = "Wednesday";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+//     case 4:
+//         newText.textContent = "Thursday";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+//     case 5:
+//         newText.textContent = "Friday";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+//     case 6:
+//         newText.textContent = "Saturday";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+//     case 7:
+//         newText.textContent = "Sunday";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//         break;
+ 
+//     default:
+//         newText.textContent = "notogri raqam";
+//         document.body.appendChild(newText)
+//         newText.style.backgroundColor = "aqua";
+//         newText.style.color = "yellow";
+//         newText.style.textAlign = "center";
+//          break;
+// }
+
+
+let userName = +prompt("Введите число от 1 до 7")
+
+switch(userName){
+    case 1:
+        alert("Monday");
+        break;
+    case 2:
+        alert("Tuesday");
+        break;
+    case 3:
+        alert("Wednesday");
+        break;
+    case 4:
+        alert("Thursday");
+        break;
+    case 5:
+        alert("Friday");
+        break;
+    case 6:
+        alert("Saturday");
+        break;
+    case 7:
+        alert("Sunday");
+        break;
+    default:
+        alert("notogri raqam");
+        break;
+}
